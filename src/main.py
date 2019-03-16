4b382937f84ccac1473426d86380b5526e1e1067
Juan Cardoso <juancardoso@outlook.com>
2019-03-16 00:59:36 -0300

    load e change image~

--- src/main.py
+++ src/main.py
@@ -6,7 +6,19 @@ PATH = "../assets"
 
 def main():
     imgs = loadImages()
-    
+    untilIndex = len(imgs) - 1
+    i = 0
+    while True:
+        cv2.imshow('image', imgs[i])
+        
+        if(i < untilIndex):
+            i+=1
+        else:
+            i = 0
+        
+        if cv2.waitKey(1000) == ord('q'):
+            break
+        
     
 
 def loadImages():
