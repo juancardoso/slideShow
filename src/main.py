3a5c8f63147b60ab14b51e773c4c1babf0456917
Juan Cardoso <juancardoso@outlook.com>
2019-03-17 20:03:56 -0300

    Add fade in images

--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
 from os import listdir
 from os.path import isfile, join
 import cv2
+import time
 
 PATH = "../assets"
+FADE_TIME = 0.07
+CV2_SHOW_NAME = 'image'
 
 def main():
     imgs = loadImages()
     untilIndex = len(imgs) - 1
     i = 0
     while True:
-        cv2.imshow('image', imgs[i])
+        cv2.imshow(CV2_SHOW_NAME, imgs[i])
         
+        before = i
         if(i < untilIndex):
             i+=1
         else:
@@ -19,6 +23,16 @@ def main():
         if cv2.waitKey(1000) == ord('q'):
             break
         
+        transitionFade(imgs[before],imgs[i])
+
+def transitionFade(fadeOut, fadeIn):
+    for x in range(10):
+        fade = x/10.0
+        dst = cv2.addWeighted(fadeOut, 1-fade, fadeIn, fade, 0)
+        cv2.imshow(CV2_SHOW_NAME, dst)
+        cv2.waitKey(1)
+        time.sleep(FADE_TIME)
+
     
 
 def loadImages():
