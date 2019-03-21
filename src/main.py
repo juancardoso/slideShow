a2dd24d3aa209b548c16fa0cebfda63783e9c7b0
Juan Cardoso <juancardoso@outlook.com>
2019-03-21 20:04:51 -0300

    Add huba

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from os import listdir
 from os.path import isfile, join
 import cv2
 import time
+import numpy as np
 
 PATH = "../assets"
 FADE_TIME = 0.07
@@ -9,10 +10,13 @@ CV2_SHOW_NAME = 'image'
 
 def main():
     imgs = loadImages()
+    imgs = applyBorder(imgs)
+    waterMark = cv2.imread('../spacex.png', cv2.IMREAD_UNCHANGED)
+    waterMark = cv2.resize(waterMark, (50, 50))
+    print(waterMark.shape)
     untilIndex = len(imgs) - 1
-    i = 0
+    i = -1
     while True:
-        cv2.imshow(CV2_SHOW_NAME, imgs[i])
         
         before = i
         if(i < untilIndex):
@@ -20,23 +24,34 @@ def main():
         else:
             i = 0
         
-        if cv2.waitKey(1000) == ord('q'):
+        transitionFade(imgs[before],imgs[i], waterMark)
+        
+        if cv2.waitKey(2000) == ord('q'):
             break
         
-        transitionFade(imgs[before],imgs[i])
 
-def transitionFade(fadeOut, fadeIn):
+def transitionFade(fadeOut, fadeIn, waterMark):
     for x in range(10):
         fade = x/10.0
         dst = cv2.addWeighted(fadeOut, 1-fade, fadeIn, fade, 0)
+        #print(dst[120:170,20:70].shape)
+        dst[120:170,20:70] = cv2.addWeighted(dst[120:170,20:70], .7, waterMark, .3, 0)
         cv2.imshow(CV2_SHOW_NAME, dst)
-        cv2.waitKey(1)
-        time.sleep(FADE_TIME)
+        if cv2.waitKey(100) == ord('q'):
+            exit(0)
 
+def applyBorder(imgs):
+    return [cv2.copyMakeBorder(img,20,20,20,20,cv2.BORDER_CONSTANT,value=(0, 0, 255)) for img in imgs]
     
 
 def loadImages():
-    return [cv2.imread(join(PATH,f)) for f in listdir(PATH) if isfile(join(PATH,f))]
+    imgs = [cv2.imread(join(PATH,f), cv2.IMREAD_UNCHANGED) for f in listdir(PATH) if isfile(join(PATH,f))]
+    for i in range(len(imgs)):
+        img = imgs[i]
+        b_channel, g_channel, r_channel = cv2.split(img)
+        alpha_channel = np.ones(b_channel.shape, dtype=b_channel.dtype) * 50 
+        imgs[i] = cv2.merge((b_channel, g_channel, r_channel, alpha_channel))
+    return imgs
 
 
 if __name__ == '__main__':
